73b216025e96b3a24369abdbf850ee1a490379fc
Hassan079 <hn178139@gmail.com>
2021-09-19 20:04:23 +0500

    Commit

--- app.js
+++ app.js
@@ -57,36 +57,36 @@ const setData = (user) => {
     });
 }
 
-    // JS For Balloons
-    let popped = 0;
+// JS For Balloons
+let popped = 0;
 
-    document.addEventListener('mouseover', function (e) {
+document.addEventListener('mouseover', function (e) {
 
-        if (e.target.className === "balloon") {
+    if (e.target.className === "balloon") {
 
-            e.target.style.backgroundColor = "#ededed";
-            e.target.textContent = "POP!";
-            popped++;
-            removeEvent(e);
-            checkAllPopped();
-        }
-    });
+        e.target.style.backgroundColor = "#ededed";
+        e.target.textContent = "POP!";
+        popped++;
+        removeEvent(e);
+        checkAllPopped();
+    }
+});
 
-    function removeEvent(e) {
-        e.target.removeEventListener('mouseover', function () {
+function removeEvent(e) {
+    e.target.removeEventListener('mouseover', function () {
 
-        })
-    };
+    })
+};
 
-    function checkAllPopped() {
-        if (popped === 16) {
-            console.log('all popped!');
-            let gallery = document.querySelector('#balloon-gallery');
-            let message = document.querySelector('#yay-no-balloons');
-            gallery.innerHTML = '';
-            message.style.display = 'block';
-        }
-    };
+function checkAllPopped() {
+    if (popped === 16) {
+        console.log('all popped!');
+        let gallery = document.querySelector('#balloon-gallery');
+        let message = document.querySelector('#yay-no-balloons');
+        gallery.innerHTML = '';
+        message.style.display = 'block';
+    }
+};
 
 
 
